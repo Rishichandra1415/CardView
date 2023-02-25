4cd0412b20b69cb53585b448c85f7d049178b4f1
rishi <rishichandra1415@gmail.com>
2023-02-25 23:44:02 +0530

    card view ...

---
+++ app/src/main/java/com/example/contactus/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.contactus;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
+
+
+// in this  we can us card view by using tag and attribuute ....
